0f69b9cb0f9c8b2492456d6f36324a4878e580a1
LebsterFace <44923609+LebsterFace@users.noreply.github.com>
2020-12-17 17:48:33 +0000

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,13 @@
+data:extend({
+  {
+    type = "custom-input",
+    name = "bluebuild-autobuild",
+    key_sequence = "SHIFT + B",
+    consuming = "none"
+  },{
+    type = "custom-input",
+    name = "bluebuild-autodemo",
+    key_sequence = "SHIFT + N",
+    consuming = "none"
+  }
+ })
